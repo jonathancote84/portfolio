c4524d67b0a9cf69d1d528a56b4f2f61d5c4a8bb
jonathancote84 <jonathancote92@gmail.com>
2019-03-26 10:20:03 -0500

    reworked file structure

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('#hamburger').click(function(){
+		$(this).toggleClass('open');
+		$('#links').toggleClass('open');
+
+	
+	});
+});
+
+
+
+
+	// $(this).addClass("click");
+    //     $('ul:first',this).css('visibility', 'visible');
+    
+    // }, function(){
+    
+    //     $(this).removeClass("click");
+    //     $('ul:first',this).css('visibility', 'hidden');
+	
